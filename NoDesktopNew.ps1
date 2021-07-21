754e99fa645217abd4383931bf61d7245b3ba1f4
Sheahan Kularathne <58463211+sheahandc@users.noreply.github.com>
2021-07-21 10:06:27 +0530

    Add files via upload

---
+++ NoDesktopNew.ps1
@@ -0,0 +1,15 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
+$RegKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies"
+If(Test-Path ($RegKey + "\Explorer"))
+{
+ $RegKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+  ##Enabled
+  New-ItemProperty -path $RegKey -name NoDesktop -value 1 -PropertyType DWord -Force
+}
+else
+{
+ New-Item -path $RegKey -name Explorer
+ $RegKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+  ##Enabled
+  New-ItemProperty -path $RegKey -name NoDesktop -value 1 -PropertyType String
+}
