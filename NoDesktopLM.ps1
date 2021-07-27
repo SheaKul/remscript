66e81aa388b11d5337e872b6a5615714c60ad751
Sheahan Kularathne <58463211+sheahandc@users.noreply.github.com>
2021-07-27 11:31:07 +0530

    Add files via upload

---
+++ NoDesktopLM.ps1
@@ -0,0 +1,16 @@
+﻿Set-PSDebug -Trace 1
+Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
+$RegKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies"
+If(Test-Path ($RegKey + "\Explorer"))
+{
+ $RegKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+  ##Enabled
+  New-ItemProperty -path $RegKey -name NoDesktop -value 1 -PropertyType DWord -Force
+}
+else
+{
+ New-Item -path $RegKey -name Explorer
+ $RegKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+  ##Enabled
+  New-ItemProperty -path $RegKey -name NoDesktop -value 1 -PropertyType String
+}
